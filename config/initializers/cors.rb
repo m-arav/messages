b21546f3a968b99592aab2c3b830941031894118
Aravind Murali <thearavindmurali@gmail.com>
2025-06-07 22:31:38 -0400

    Add cors for angular traget

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,14 +5,13 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     # Todo change to deploy target
-#     origins "example.com"
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "messages-ui-zkd1.onrender.com"
 
-#     resource "*",
-#       headers: :any,
-#       expose: [ "Authorization" ],
-#       methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
-#   end
-# end
+    resource "*",
+      headers: :any,
+      expose: [ "Authorization" ],
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+  end
+end
