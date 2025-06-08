93348464b753636dddd08194cb30196b6d0bc5ba
Aravind Murali <thearavindmurali@gmail.com>
2025-06-08 01:06:05 -0400

    fix webhook

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,8 +3,7 @@ class TwilioController < ApplicationController
     sid = params["MessageSid"]
     status = params["MessageStatus"]
 
-    Message.find(sid: sid)
-    message.update(status: status)
+    Message.find(sid: sid).then { |message| message.update(status: status) }
   rescue => e
     Rails.logger.error("[Twilio Webhook Error] #{e.class}: #{e.message}")
     head :ok
